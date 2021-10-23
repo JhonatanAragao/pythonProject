2173e68be7e93937c340d2177fc4713eb209face
JhonatanAragao <ajhonatamslash@gmail.com>
2021-10-23 12:08:31 -0300

    Jokenpô

---
+++ Pokemons.py
@@ -0,0 +1,25 @@
+#PROBLEMAS
+
+#Ash Ketchum é um jovem da cidade de Pallet que está
+#viajando pelo país em sua aventura de tornar-se o
+#melhor treinador pokémon. No caminho ele participou
+#de muitas batalhas e tem um grande número de
+#pokémons, tantos que já é bem difícil saber quantos e
+#quais são e frequentemente ele se embaraça achando
+#que possui um pokémon quando não o possui, ou
+#esquecendo os que possui.
+#Para resolver este problema, Ash pediu a você que o ajudasse criando um programa que
+#o auxilie a gerenciar melhor seus pokémons. O programa deve exibir um menu com as
+#opções:
+#1. Adicionar pokémon
+#2. Remover pokémon
+#3. Listar pokémons
+#4. Mostrar pokémons por letra inicial
+#5. Sair
+#As opções 3 e 4 deve exibir os itens e mostrar a quantidade de itens exibidos (ex.: “37
+#pokémons encontrados”). A opção 4 deve receber uma letra do usuário e mostrar todos
+#os itens da lista que iniciam com essa letra (maiúsculas e minúsculas devem ser
+#ignoradas).
+
+menu = ("1. Adicionar pokémon\n2. Remover pokémon\n3. Listas\n4. Mostrar pokémons por letra inicial\n5. Sair")#criação do menu de interação!
+print(menu)
